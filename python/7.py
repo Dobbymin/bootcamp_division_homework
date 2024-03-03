b5330c1a0a3afa9743bfbf8ca69219e86570401b
Dobbymin <gmin0701@knu.ac.kr>
2024-03-03 18:21:15 +0900

    해달 과제 - python

--- python/7.py
+++ python/7.py
@@ -15,7 +15,13 @@ def main():
             print(30)
         else:
             print(31)
-
+    else:
+        if month == 2:
+            print(28)
+        elif month == 4 or month == 6 or month == 9 or month == 11:
+            print(30)
+        else:
+            print(31)
     return
 
 
